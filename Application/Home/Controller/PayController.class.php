4eb621f372b9e0bfc6bdc7d3306aab11b96420ce
余济舟-家 <1045135632@qq.com>
2016-06-17 20:53:59 +0800

    测试：BC返回值

--- Application/Home/Controller/PayController.class.php
+++ Application/Home/Controller/PayController.class.php
@@ -179,101 +179,110 @@ class PayController extends Controller
 		}
 	}
 
+	public function bcWebHookTest()
+	{
+		$appId = C('bcAppId');
+		$appSecret = C('bcAppSecret');
+		$jsonStr = file_get_contents('php://input');
+		$msg = json_decode($jsonStr, 1);
+		a4saveFile2Json('bcWebHookTest.json', $msg);
+	}
+
 	/**
 	 * @todo 接受BcWebHook
 	 */
 	public function bcWebHook()
 	{
-			/**
-			 * http类型为 Application/json, 非XMLHttpRequest的application/x-www-form-urlencoded, $_POST方式是不能获取到的
-			 */
-			$appId = C('bcAppId');
-			$appSecret = C('bcAppSecret');
-			$jsonStr = file_get_contents("php://input");
-			$msg = json_decode($jsonStr);
-			a4saveFile2Json('./wechatPayed.json',json_encode($msg));
-			// webhook字段文档: https://beecloud.cn/doc/?index=webhook
-			//第一步:验证签名
-			$sign = md5($appId . $appSecret . $msg->timestamp);
-			if ($sign != $msg->sign) {
-				// 签名不正确
-				echo('success');
-				exit();
-			}
-			//第二步:过滤重复的Webhook
-			//客户需要根据订单号进行判重，忽略已经处理过的订单号对应的Webhook
-			if ($msg->transaction_id == $_SESSION['sys']['pay']['orderFormInfo']['no']) {
-				echo('success');
-				exit();
-			}
-			//
-			//第三步:验证订单金额与购买的产品实际金额是否一致
-			//也就是验证调用Webhook返回的transaction_fee订单金额是否与客户服务端内部的数据库查询得到对应的产品的金额是否相同
+		/**
+		 * http类型为 Application/json, 非XMLHttpRequest的application/x-www-form-urlencoded, $_POST方式是不能获取到的
+		 */
+		$appId = C('bcAppId');
+		$appSecret = C('bcAppSecret');
+		$jsonStr = file_get_contents("php://input");
+		$msg = json_decode($jsonStr);
+		a4saveFile2Json('./wechatPayed.json', json_encode($msg));
+		// webhook字段文档: https://beecloud.cn/doc/?index=webhook
+		//第一步:验证签名
+		$sign = md5($appId . $appSecret . $msg->timestamp);
+		if ($sign != $msg->sign) {
+			// 签名不正确
+			echo('success');
+			exit();
+		}
+		//第二步:过滤重复的Webhook
+		//客户需要根据订单号进行判重，忽略已经处理过的订单号对应的Webhook
+		if ($msg->transaction_id == $_SESSION['sys']['pay']['orderFormInfo']['no']) {
+			echo('success');
+			exit();
+		}
+		//
+		//第三步:验证订单金额与购买的产品实际金额是否一致
+		//也就是验证调用Webhook返回的transaction_fee订单金额是否与客户服务端内部的数据库查询得到对应的产品的金额是否相同
 //			if ($msg->transaction_fee != $_SESSION['sys']['pay']['orderFormInfo']['price']) {
 //				echo('success');
 //				exit();
 //			}
-			//第四步:处理业务逻辑和返回
-			if ($msg->transaction_type == "PAY") { //支付的结果
-				//付款信息
-				//支付状态是否变为支付成功,true代表成功
-				$result = $msg->trade_success;
-				if ($msg->trade_success != true) {
-					echo('success');
-					exit;
-				}
-				//message_detail 参考文档
-				//channel_type 微信/支付宝/银联/快钱/京东/百度/易宝/PAYPAL
-				switch ($msg->channel_type) {
-					case "WX":
-						echo 'success';
-						a4saveFile2Json('./wechatPayed.json',json_encode($msg));
-						/**
-						 * 处理业务
-						 */
-						#修改订单支付状态
-						$of = new OrderFormModel();
-						$of->updateFOrderFormStatusId_byNo($msg->transaction_id, 2);
-						#修改BC订单号
-						$of->updateBcId_byNo($msg->transaction_id, $msg->id);
-						#获取订单信息
-						$ofInfo = $of->get_byNo($msg->transaction_id);
-						#发送模板信息（通知库管）
-						$wx = new \a4Wechat();
-						$at = $wx->getAccessToken();
-						$conf = a4getConf();
-						$data['touser'] = 'ojX0NwOqkO4sifGfEPh8f0MrVpjE';
-						$data['template_id'] = $conf['wechat']['templateMsg']['新订单'];
-						$data['url'] = 'http://www.phphelper.cn/bs.php/home/pay/detail.html?no=' . $msg->transaction_id;
-						$data['topcolor'] = '#0000FF';
-						$data['data'] = array(
-							'time' => array('value' => a4getNow('datetime'), 'color' => '#404040'),
-							'userInfo' => array('value' => $ofInfo['username'], 'color' => '#009900'),
-							'no' => array('value' => $msg->transaction_id, 'color' => '#990099')
-						);
-						$res = a4post($conf['wechat']['getTemplateMsgIdUrl'] . $at, json_encode($data));
-						break;
-					case "ALI":
-						break;
-					case "UN":
-						break;
-					case "KUAIQIAN":
-						break;
-					case "JD":
-						break;
-					case "BD":
-						break;
-					case "YEE":
-						break;
-					case "PAYPAL":
-						break;
-				}
-			} else if ($msg->transaction_type == "REFUND") { //退款的结果
+		//第四步:处理业务逻辑和返回
+		if ($msg->transaction_type == "PAY") { //支付的结果
+			//付款信息
+			//支付状态是否变为支付成功,true代表成功
+			$result = $msg->trade_success;
+			if ($msg->trade_success != true) {
+				echo('success');
+				exit;
 			}
+			//message_detail 参考文档
+			//channel_type 微信/支付宝/银联/快钱/京东/百度/易宝/PAYPAL
+			switch ($msg->channel_type) {
+				case "WX":
+					echo 'success';
+					a4saveFile2Json('./wechatPayed.json', json_encode($msg));
+					/**
+					 * 处理业务
+					 */
+					#修改订单支付状态
+					$of = new OrderFormModel();
+					$of->updateFOrderFormStatusId_byNo($msg->transaction_id, 2);
+					#修改BC订单号
+					$of->updateBcId_byNo($msg->transaction_id, $msg->id);
+					#获取订单信息
+					$ofInfo = $of->get_byNo($msg->transaction_id);
+					#发送模板信息（通知库管）
+					$wx = new \a4Wechat();
+					$at = $wx->getAccessToken();
+					$conf = a4getConf();
+					$data['touser'] = 'ojX0NwOqkO4sifGfEPh8f0MrVpjE';
+					$data['template_id'] = $conf['wechat']['templateMsg']['新订单'];
+					$data['url'] = 'http://www.phphelper.cn/bs.php/home/pay/detail.html?no=' . $msg->transaction_id;
+					$data['topcolor'] = '#0000FF';
+					$data['data'] = array(
+						'time' => array('value' => a4getNow('datetime'), 'color' => '#404040'),
+						'userInfo' => array('value' => $ofInfo['username'], 'color' => '#009900'),
+						'no' => array('value' => $msg->transaction_id, 'color' => '#990099')
+					);
+					$res = a4post($conf['wechat']['getTemplateMsgIdUrl'] . $at, json_encode($data));
+					break;
+				case "ALI":
+					break;
+				case "UN":
+					break;
+				case "KUAIQIAN":
+					break;
+				case "JD":
+					break;
+				case "BD":
+					break;
+				case "YEE":
+					break;
+				case "PAYPAL":
+					break;
+			}
+		} else if ($msg->transaction_type == "REFUND") { //退款的结果
+		}
 //打印所有字段
 //			print_r($msg);
 //处理消息成功,不需要持续通知此消息返回success
-			echo 'success';
+		echo 'success';
 
 	}
 }
